3c10654fc679b1f16b32d0ed91d8552f9f6f81ca
firetending <alan.miller.375@gmail.com>
2022-07-08 02:56:17 -0400

    satellite type counts

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -2,30 +2,45 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Satellite } from '../satellite';
 
 @Component({
-  selector: 'app-orbit-counts',
-  templateUrl: './orbit-counts.component.html',
-  styleUrls: ['./orbit-counts.component.css']
+	selector: 'app-orbit-counts',
+	templateUrl: './orbit-counts.component.html',
+	styleUrls: ['./orbit-counts.component.css']
 })
 export class OrbitCountsComponent implements OnInit {
 
 	@Input() satellites: Satellite[];
+	typesList: any[] = [];
 
-  constructor() { }
+  constructor() {
+	// this.typesList = [];
+  }
 
   ngOnInit() {
   }
 
-  countByType(type: string): number {
-	let count = 0;
-	if (this.satellites) {
-	  for (let i = 0; i < this.satellites.length; i++) {
-		 if (this.satellites[i].type === type) {
-			count++;
-		 }
-	  }
+  ngOnChanges() {
+	this.typesList = this.getTypes(this.satellites);
+  }
+
+	countByType(type: string): number {
+		let count = 0;
+		if (this.satellites) {
+			for (let i = 0; i < this.satellites.length; i++) {
+				if (this.satellites[i].type === type) {
+					count++;
+				}
+			}
+		}
+		return count;
 	}
-	return count;
- }
 
-
+	getTypes(array: Satellite[]): any[] {
+		let temp: any[] = [];
+		for (let satellite of array) {
+			if (!temp.includes(satellite.type)) {
+				temp.push(satellite.type);
+			}
+		}
+		return temp;
+	}
 }
